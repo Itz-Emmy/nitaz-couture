fe1ab6d3d0e2d4b5c18715e7c69146c61fec523f
Emmanuel Agbonyeme <nereus4luv@yahoo.com>
2020-07-11 23:13:01 +0100

    vh fix not working

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// Fixing vh issue for all browser
-//First we get the viewport height and we multiply it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("50vh", `${vh}px`);
-// We listen to the resize event
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("50vh", `${vh}px`);
-});
-
 //Menu bar function
 function myFunction() {
   var x = document.getElementById("nav-bar");
